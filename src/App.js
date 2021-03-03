f5fc5f036316e520b1cae072186e6ab8c3992618
Sissi <sissi.stav@gmail.com>
2021-03-03 17:06:46 +0200

    update our menu-items to navigate

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import {Switch, Route} from 'react-router-dom';
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE </h1>
+  </div>
+);
 
 class App extends React.Component {
   render() {
     return (
     <div className='App'>
-        <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/shop/hats' component={HatsPage} />
+      </Switch>
       </div>
   );
  }
