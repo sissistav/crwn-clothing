6e586e12a56c6bf9c6afbd75580eec5442e415fd
Sissi <sissi.stav@gmail.com>
2021-04-27 16:02:22 +0300

    add-collection-item-component-and-style

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import {Switch, Route} from 'react-router-dom';
+import ShopPage from './pages/shop/shop.component';
 
 const HatsPage = () => (
   <div>
@@ -15,7 +16,7 @@ class App extends React.Component {
     <div className='App'>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route exact path='/shop/hats' component={HatsPage} />
+        <Route exact path='/shop' component={ShopPage} />
       </Switch>
       </div>
   );
